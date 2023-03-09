bcb3bfedb5107b960a85d818e960020298e25bcd
Timmy <okunolatimilehin4123@gmail.com>
2023-03-09 17:51:35 +0100

    Authentication testing done

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -13,4 +13,12 @@ export class PrismaService extends PrismaClient {
       },
     });
   }
+
+  //This logic runs for e2e test
+  cleanDb() {
+    return this.$transaction([
+      this.bookmark.deleteMany(),
+      this.user.deleteMany(),
+    ]);
+  }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,128 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { ValidationPipe, Post } from '@nestjs/common';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import * as pactum from 'pactum';
+import { AuthDto } from '../src/auth/dto';
 
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+    await app.listen(3334);
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3334');
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
   });
+
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'okunolatimilehin4123@gmail.com',
+      password: 'Timmy12345',
+    };
+    describe('Sign Up', () => {
+      it('To Throw error if email is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('To Throw error if password is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+      it('To Throw error if dto is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({})
+          .expectStatus(400);
+      });
+
+      it('To Sign up', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('Sign In', () => {
+      it('To Throw error if email is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('To Throw error if password is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+      it('To Throw error if dto is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({})
+          .expectStatus(400);
+      });
+
+      it('To sign in', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200);
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('Get Me', () => {});
+
+    describe('Edit User', () => {});
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create Bookmark', () => {});
+    describe('Get Bookmarks', () => {});
+    describe('Get Bookmarks by Id', () => {});
+    describe('Update Bookmarks', () => {});
+    describe('Delete Bookmarks', () => {});
+  });
+  // it.todo('To Pass');
 });
